2b67f2d3d061b144230356edf2068b416bd62cf5
Payin14 <payin630@gmail.com>
2024-02-24 23:43:59 +0000

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - checker
+ * @n: The number to check
+ * Description: check the sign of the number
+ * Return: 1 if is greater than zero, 0 if is zero
+ * -1 if is less than zero
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
