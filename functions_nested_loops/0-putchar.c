d625f6fe794c296fde6a09f5bd1ab6b4430774c2
Payin14 <payin630@gmail.com>
2024-02-24 23:33:39 +0000

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * main - main code
+ * Description: call the _putchar funciton
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
