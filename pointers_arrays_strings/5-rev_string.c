8dbc7d429a9f9fc8b1f9bdf00190f25dfa377dfd
Payin14 <payin630@gmail.com>
2024-03-02 23:11:56 +0000

    all

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: string to reverse
+ * Return: empty
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	int k = 0;
+	int aux;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	k = i - 1; /*lenght of s without \0 but in format position*/
+
+	for (j = 0; j < (i / 2); j++)
+	{
+		aux = s[j];
+		s[j] = s[k];
+		s[k] = aux;
+		k--;
+	}
+
+}
