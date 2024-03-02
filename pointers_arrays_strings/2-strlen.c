85ca580a1700f8136b74dea6a3976b21dc61b6c9
Payin14 <payin630@gmail.com>
2024-03-02 23:09:14 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: variable string to check the size
+ * Return: an integer with the size
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
